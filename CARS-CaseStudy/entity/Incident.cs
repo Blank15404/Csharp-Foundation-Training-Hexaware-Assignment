f9c201b7824b611fa831ebdb9f78f40dee3ce75a
Manish Sivakumar <157442586+Blank15404@users.noreply.github.com>
2025-04-21 10:46:47 +0530

    Add files via upload

---
+++ CARS-CaseStudy/entity/Incident.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARS_CaseStudy.entity
+{
+    public class Incident
+    {
+        public int IncidentID { get; set; }
+        public string IncidentType { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int VictimID { get; set; }
+        public int SuspectID { get; set; }
+
+        public Incident() { }
+
+        public Incident(int incidentID, string incidentType, DateTime incidentDate, string location,
+                       string description, string status, int victimID, int suspectID)
+        {
+            IncidentID = incidentID;
+            IncidentType = incidentType;
+            IncidentDate = incidentDate;
+            Location = location;
+            Description = description;
+            Status = status;
+            VictimID = victimID;
+            SuspectID = suspectID;
+        }
+    }
+}
