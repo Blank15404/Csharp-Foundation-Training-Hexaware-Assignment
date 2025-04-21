aa57d117f114993f5b790a086339f1505802fa43
Manish Sivakumar <157442586+Blank15404@users.noreply.github.com>
2025-04-21 10:48:17 +0530

    Create IncidentNumberNotFoundException.cs

---
+++ CARS-CaseStudy/exception/IncidentNumberNotFoundException.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARS_CaseStudy.exception
+{
+    public class IncidentNumberNotFoundException : Exception
+    {
+        public IncidentNumberNotFoundException() : base("Incident number not found in the database.") { }
+
+        public IncidentNumberNotFoundException(string message) : base(message) { }
+
+        public IncidentNumberNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
