bc34d1f47d1723f570e6368a94f6795f07244b1b
Manish Sivakumar <157442586+Blank15404@users.noreply.github.com>
2025-04-21 10:49:51 +0530

    Add files via upload

---
+++ CARS-CaseStudy/IncidentServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using CARS_CaseStudy.dao;
+using CARS_CaseStudy.entity;
+using CARS_CaseStudy.exception;
+
+namespace CARSTestProject
+{
+    [TestFixture]
+    public class IncidentServiceTests
+    {
+        private ICrimeAnalysisService incidentService;
+
+        [SetUp]
+        public void Setup()
+        {
+            incidentService = new CrimeAnalysisServiceImpl();
+        }
+
+        [Test]
+        public void CreateIncident_ValidData_ReturnsTrue()
+        {
+            var incident = new Incident(3000, "Theft", new DateTime(2023, 1, 1),
+                           "Main St", "Stolen wallet", "Open", 1001, 2001);
+
+            bool result = incidentService.CreateIncident(incident);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void UpdateIncidentStatus_ValidId_ReturnsTrue()
+        {
+            incidentService.CreateIncident(
+                new Incident(3002, "Burglary", new DateTime(2023, 1, 2),
+                "Park Ave", "Broken window", "Open", 1001, 2001));
+
+            bool result = incidentService.UpdateIncidentStatus("Closed", 3002);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void GetIncidentsInDateRange_ReturnsCorrectIncidents()
+        {
+            incidentService.CreateIncident(
+                new Incident(3003, "Vandalism", new DateTime(2023, 1, 3),
+                "School", "Graffiti", "Open", 1001, 2001));
+
+            var incidents = incidentService.GetIncidentsInDateRange(
+                new DateTime(2023, 1, 1),
+                new DateTime(2023, 1, 31));
+
+            Assert.IsNotEmpty(incidents);
+        }
+
+        [Test]
+        public void SearchIncidents_ReturnsMatchingResults()
+        {
+            incidentService.CreateIncident(
+                new Incident(3004, "Assault", new DateTime(2023, 1, 4),
+                "Bar", "Bar fight", "Open", 1001, 2001));
+
+            var results = incidentService.SearchIncidents("Assault");
+            Assert.IsNotEmpty(results);
+        }
+    }
+}
