5dad8e60a8744a7ee61c454fc1184f42a89b8403
Manish Sivakumar <157442586+Blank15404@users.noreply.github.com>
2025-04-21 10:45:24 +0530

    Add files via upload

---
+++ CARS-CaseStudy/dao/ICrimeAnalysisService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using CARS_CaseStudy.entity;
+
+namespace CARS_CaseStudy.dao
+{
+    public interface ICrimeAnalysisService
+    {
+        bool CreateIncident(Incident incident);
+        bool UpdateIncidentStatus(string status, int incidentId);
+        List<Incident> GetIncidentsInDateRange(DateTime startDate, DateTime endDate);
+        List<Incident> SearchIncidents(string criteria);
+        Report GenerateIncidentReport(Report report);
+        Case CreateCase(string caseDescription, List<Incident> incidents, int caseId);
+        Case GetCaseDetails(int caseId);
+        bool UpdateCaseDetails(Case caseObj);
+        List<Case> GetAllCases();
+    }
+}
