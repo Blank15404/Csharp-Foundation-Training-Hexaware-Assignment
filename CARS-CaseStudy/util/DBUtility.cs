0b0ff1bf06c0f599629e5d77cefb2c65b59c2408
Manish Sivakumar <157442586+Blank15404@users.noreply.github.com>
2025-04-24 19:14:30 +0530

    Add files via upload

---
+++ CARS-CaseStudy/util/DBUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CARS_CaseStudy.util
+{
+    public static class DBUtility
+    {
+        static readonly string connectionString = @"Server=DESKTOP-F473ICG\SQLEXPRESS;Database=CrimeAnalysisDB;Integrated Security=True;MultipleActiveResultSets=true;";
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection ConnectionObject = new SqlConnection(connectionString);
+            try
+            {
+                ConnectionObject.Open();
+                return ConnectionObject;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Opening the Connection : {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
